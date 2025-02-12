054e522cd234c7cead3814b1bd2c3b171bf4a62a
agnesnthabisengm@gmail.com <agnesnthabisengm@gmail.com>
2025-02-12 09:39:45 +0200

    testing

--- booksdb.session.sql
+++ booksdb.session.sql
@@ -1,1 +1,8 @@
-SELECT id, book_id, availability FROM books WHERE book_id = 'RQ6xDwAAQBAJ';  -- Example
+-- SELECT id, book_id, availability FROM books WHERE book_id = 'RQ6xDwAAQBAJ';  
+-- DELETE FROM books WHERE user_id = 1;
+-- DELETE FROM categories  
+-- WHERE category_id = 203;
+
+-- WHERE category_name IN ('mathematics', 'maths', 'physics', 'Romance', 'christain', 'Comp', 'Statistics', 'animal');
+-- DELETE FROM categories
+-- WHERE category_name IN ('Chemistry', 'Statistics')
